2193f3c38c02e39db444220d5b8d5b912b6cb30a
seenso <>
2022-01-13 14:03:34 -0900

    commit tree 511b94fdc0b7f9e18ad3478f7dbaaf4b8a5c96ff

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,11 @@ class PostsController < ApplicationController
 
   def show
     post = Post.find(params[:id])
-    render json: post
+    if post 
+      render json: post
+    else 
+      render json: { error: "Post not found"}
+    end
   end
 
   private
